f91803ba8c9c1eb2ede0772cc30968f9aea6ec5c
Fidot02 <emekafelixabojei@gmail.com>
2025-04-13 23:52:48 +0100

    Exceptions

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+//EXCEPTION HANDLING
+
+try
+{
+    Console.WriteLine("Enter a number to divide 100 by:");
+    int divisor = Convert.ToInt32(Console.ReadLine());
+
+    int result = 100 / divisor;
+
+    Console.WriteLine($"Result: 100 / {divisor} = {result}");
+}
+
+catch (DivideByZeroException)
+{ Console.WriteLine("Error! Cannot divide by 0"); }
+
+catch (Exception)
+{ Console.WriteLine("Error! please input a valid number"); }
+
+Console.ReadKey();
